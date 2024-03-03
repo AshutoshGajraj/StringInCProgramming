c852739485030acbe0ed8affcce2510e6becfb05
AshutoshGajraj <gajrajashutosh23@gmail.com>
2024-03-03 12:03:11 +0545

    Commiting input

---
+++ string_input.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+main(){
+    char name[9] = "Ashutosh";      // size of charater array should be 1 more than length of string as last character is null character '\0'
+    printf("Name = %s ", name);
+}   
